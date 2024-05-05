bc5de4e35e4840abbde71cfdef540db6a5a56ed7
salazam14 <138829539+salazam14@users.noreply.github.com>
2024-05-04 22:19:45 -0400

    Delete Final Program Code directory

--- Final Program Code/CheckInput.java
+++
@@ -1,6 +0,0 @@
-public class CheckInput {
-    public static boolean validate(String username, String password) {
-        return password.length() >= 12 && password.matches(".*[a-z].*") && 
-        		password.matches(".*[A-Z].*") && password.matches(".*[!@#$%^&*].*");
-    }
-}
